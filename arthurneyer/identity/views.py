7c0a2a15b08f3c93897138f716bee736d173ccd9
GoswaTech <arthur.neyer@protonmail.com>
2021-04-08 18:57:08 +0200

    Edit xframe of views

--- arthurneyer/identity/views.py
+++ arthurneyer/identity/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from django.views.decorators.clickjacking import xframe_options_exempt
 
 from .models import Portfolio
 
 
 # Create your views here.
+@xframe_options_exempt
 def home(request):
 
     portfolio = Portfolio.get_all_portfolios()
