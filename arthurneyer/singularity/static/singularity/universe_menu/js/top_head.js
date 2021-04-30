6c11ab8f4656a7ee1f67bdb011c3b13d4a83459e
GoswaTech <arthur.neyer@protonmail.com>
2021-04-30 20:19:13 +0200

    Première version du menu univers

--- arthurneyer/singularity/static/singularity/universe_menu/js/top_head.js
+++ arthurneyer/singularity/static/singularity/universe_menu/js/top_head.js
@@ -1,3 +1,9 @@
+function addBadge(menu) {
+  var badge = document.createElement('img');
+  badge.src = document.body.dataset['badge'];
+  badge.alt = 'Badge';
+  menu.appendChild(badge);
+}
 
 function create_singularity_universe_menu(menu) {
   if (menu == null) {
@@ -5,6 +11,8 @@ function create_singularity_universe_menu(menu) {
     menu.id = "singularity_universe_menu";
     menu.classList.add('menu_space_digital');
   }
+
+  addBadge(menu);
 
   var body = document.getElementById('singularity_body');
   body.appendChild(menu);
