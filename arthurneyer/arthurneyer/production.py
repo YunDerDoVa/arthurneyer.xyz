f35c9ab8ab7c1e94aba236c11f347a77b97dd4e1
Arthur NEYER <arthur.neyer@protonmail.com>
2021-05-01 13:38:28 +0000

    Update prod settings

--- arthurneyer/arthurneyer/production.py
+++ arthurneyer/arthurneyer/production.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'identity.apps.IdentityConfig',
+    'singularity.apps.SingularityConfig',
 ]
 
 MIDDLEWARE = [
