27d62f79808b579c4b2f18002289978bffc2ebb7
Arthur NEYER <arthur.neyer@protonmail.com>
2021-05-13 12:50:28 +0000

    Fix bug

--- arthurneyer/arthurneyer/production.py
+++ arthurneyer/arthurneyer/production.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'singularity.apps.SingularityConfig',
     'identity.apps.IdentityConfig',
-    'singularity.apps.SingularityConfig',
     'greentea.apps.GreenteaConfig',
 ]
 
