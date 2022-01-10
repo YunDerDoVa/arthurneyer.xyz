b27743093b4956bc56a1bbf0c68016f4b7ea156c
3rtea <arthur.neyer@muggum.fr>
2022-01-10 18:01:20 +0100

    small maj before...

---
+++ arthurneyer/install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo "Vous avez entré la commande \"$0, $1\""
+
+if [[ $1 == 'all' ]]; then
+  #statements
+  python -m venv venv
+  source venv/bin/activate & pip install -r requirements.txt
+  cd identity/static/ & npm install
+elif [[ $1 == 'requirements' ]]; then
+  #statements
+  source venv/bin/activate & pip install -r requirements.txt
+  cd identity/static/ & npm install
+elif [[ $1 == 'statics' ]]; then
+  #statements
+  yarn install --modules-folder identity/static/node_modules/
+elif [[ $1 == 'check' ]]; then
+  #statements
+  if [[ `ls identity/static/node_modules/ >&1 2>/dev/null` ]]; then
+    #statements
+    echo "Node Modules Exists"
+  else
+    echo "No Node Modules :p"
+  fi
+  # Second Tests
+  if [[ `ls venv/bin/activate >&1 2>/dev/null` ]]; then
+    #statements
+    echo "Venv Exists"
+  else
+    echo "No Venv :p"
+  fi
+fi
